be9d81731a771c341e80b7e2a6fbea38014fe610
Margarita <margaritakrasnauska@gmail.com>
2023-02-14 16:34:14 +0000

    Add project files.

---
+++ Class_and_Method_Assignment/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_and_Method_Assignment
+{
+    static class Math
+    {
+        public static void MyMathAdd(int a = 9, int b = 7)
+        {
+            int c = (a + b) / 2;
+            Console.WriteLine("a+b/2 b=7 \nResult:" + c);
+        }
+        public static int MyMathDiv(int x, int y)
+        {
+            return x / y;
+
+        }
+        public static double MyMathDiv(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
+
---
+++ Class_and_Method_Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_and_Method_Assignment
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+
+            Math.MyMathAdd();
+            Console.WriteLine("Type number for \"a\" value");
+            Math.MyMathAdd(Convert.ToInt32(Console.ReadLine()), 7);
+
+
+            int z = Math.MyMathDiv(100, 52);
+            double m = Math.MyMathDiv(1.84, 7.4);
+            Console.WriteLine("Integer division : " + z);
+            Console.WriteLine("Double division : " + m);
+            Console.ReadLine();
+
+        }
+    }
+}
